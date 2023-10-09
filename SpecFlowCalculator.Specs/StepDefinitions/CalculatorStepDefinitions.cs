acf8c854acf3fc54b7f7ce53e11b9f316e6260c4
intrepion <Oliver.Forral@icf.com>
2023-10-09 07:36:35 -0600

    Updated step definitions.

--- SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
+++ SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,46 +1,49 @@
-namespace SpecFlowCalculator.Specs.StepDefinitions
+using CalculatorSelenium.Specs.Drivers;
+using CalculatorSelenium.Specs.PageObjects;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace CalculatorSelenium.Specs.Steps
 {
     [Binding]
     public sealed class CalculatorStepDefinitions
     {
-        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
-        private readonly Calculator _calculator = new Calculator();
-        private int _result;
+        //Page Object for Calculator
+        private readonly CalculatorPageObject _calculatorPageObject;
+
+        public CalculatorStepDefinitions(BrowserDriver browserDriver)
+        {
+            _calculatorPageObject = new CalculatorPageObject(browserDriver.Current);
+        }
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            _calculator.FirstNumber = number;
+            //delegate to Page Object
+            _calculatorPageObject.EnterFirstNumber(number.ToString());
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-
-            _calculator.SecondNumber = number;
+            //delegate to Page Object
+            _calculatorPageObject.EnterSecondNumber(number.ToString());
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
-            _result = _calculator.Add();
+            //delegate to Page Object
+            _calculatorPageObject.ClickAdd();
         }
 
         [Then("the result should be (.*)")]
-        public void ThenTheResultShouldBe(int result)
+        public void ThenTheResultShouldBe(int expectedResult)
         {
-            //TODO: implement assert (verification) logic
+            //delegate to Page Object
+            var actualResult = _calculatorPageObject.WaitForNonEmptyResult();
 
-            _result.Should().Be(result);
+            actualResult.Should().Be(expectedResult.ToString());
         }
     }
 }
